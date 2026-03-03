54551de23c1777146080cdb41a28477514acca69
Thiago F. Miranda <33705632+thiagofmiranda@users.noreply.github.com>
2026-03-03 16:45:38 -0300

    Initial commit

---
+++ dev/dev-start.R
@@ -0,0 +1,12 @@
+# Licença MIT
+usethis::use_mit_license()
+
+# Configura testthat
+usethis::use_testthat()
+
+# Adiciona dependência R6
+usethis::use_package("R6")
+
+# Inicializa Git
+usethis::use_git()
+# → Vai perguntar se quer commitar os arquivos → responde Yes
